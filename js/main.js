f73a7f076e1f63c505f0dd47ca3a991f9e498c93
Anna Peterson <annapeterson@Annas-MacBook-Pro.local>
2019-02-08 17:41:26 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*----- app's state (variables) -----*/ 
+let count = 0;
+let inputVal = 0
+
+
+/*----- cached element references -----*/ 
+
+
+
+/*----- event listeners -----*/ 
+
+document.getElementById("plus").addEventListener('click', addOne)
+document.getElementById("minus").addEventListener('click', minusOne)
+document.getElementById("input").addEventListener('change', inputNumber)
+
+
+/*----- functions -----*/
+
+function addOne() {
+    count += 1
+    render();
+};
+
+function minusOne() {
+    count += -1;
+    render();
+};
+
+function render() {
+    document.getElementById("number-clicks").innerHTML = count;
+    document.getElementById("input").value = count;
+    if (count < 0) {
+    document.getElementById("number-clicks").style.color = 'red';
+    }
+};
+
+function inputNumber(str) {
+    str = document.getElementById("input").value
+    inputVal = parseInt(str);
+    count = inputVal;
+    render();
+}
